bd8e22c4bb81845c97c1142b37e68b9d91482a84
peter020104 <100434449+peter020104@users.noreply.github.com>
2022-03-07 23:21:26 +0800

    add index PHP file

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
+
+        <?php
+            echo "Hello <h1>PHP</h1>!";
+            echo "<br/>";
+            $i=10;
+            $j=20;
+            echo $i+$j;
+            echo "<br/>";
+            echo date("Y/m/d");
+        ?>
+
+    </body>
+</html>
